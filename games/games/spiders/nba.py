3f3a3e0e840c94c7b92c6fedfb80c75915f90078
Stas Deep <glubokiy.stas@gmail.com>
2017-09-24 23:31:36 +0300

    Add extra stats

--- games/games/spiders/nba.py
+++ games/games/spiders/nba.py
@@ -20,15 +20,51 @@ class NbaSpider(scrapy.Spider):
             yield response.follow(a_tag, callback=self.parse_game)
 
     def parse_game(self, response):
-        scores = [int(x) for x in response.css('.scorebox .score::text').extract()]
-        teams = response.css('.scorebox a[itemprop="name"]::text').extract()
+        teams = [x.split('/')[2] for x in response.css('.scorebox a[itemprop="name"]::attr(href)').extract()]
+        datetime = response.css('.scorebox_meta div:first-child::text').extract_first()
+
+        # Box scores tables (+ advanced).
+        away_bs, away_bs_adv, home_bs, home_bs_adv = response.css('.stats_table tfoot')
+
+        def away_stat(stat, to=int):
+            return self.get_stat(away_bs, stat, to)
+
+        def home_stat(stat, to=int):
+            return self.get_stat(home_bs, stat, to)
+
+        def team_stats(team):
+            if team == 'away':
+                get_stat = away_stat
+            else:
+                get_stat = home_stat
+
+            stats = [
+                'pts',
+                'fg',
+                'fga',
+                'fg3',
+                'fg3a',
+                'ft',
+                'fta',
+                'orb',
+                'drb',
+                'ast',
+                'stl',
+                'blk',
+                'tov',
+                'pf',
+            ]
+
+            result = {stat_name: get_stat(stat_name) for stat_name in stats}
+            result['team_id'] = teams[0] if team == 'away' else teams[1]
+
+            return result
+
         yield {
-            'home_team': {
-                'name': teams[0],
-                'score': scores[0]
-            },
-            'guest_team': {
-                'name': teams[1],
-                'score': scores[0]
-            }
+            'away': team_stats('away'),
+            'home': team_stats('home'),
+            'datetime': datetime
         }
+
+    def get_stat(self, box_score, stat, to):
+        return to(box_score.css('td[data-stat="{}"]::text'.format(stat)).extract_first())
